baa09264322a53f9ddc17adfe142078f8cf50125
Sébastien Butler <sebastien.butler@gmail.com>
2016-04-26 22:14:16 +0200

    test commit

---
+++ Source/TestUEGit/TestUEGitGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TestUEGitGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTUEGIT_API ATestUEGitGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
